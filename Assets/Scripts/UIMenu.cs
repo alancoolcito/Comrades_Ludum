201d08dc724ae331df8f40818b8afce5c4dbf649
alancoolcito <alaneroperel@gmail.com>
2021-02-01 20:31:58 +0200

    +1

---
+++ Assets/Scripts/UIMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+
+    }
+
+    public void StartGame()
+    {
+        FindObjectOfType<SceneFader>().FadeTo(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
